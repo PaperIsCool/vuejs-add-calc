e7e31d1513b89c46ac97afa06ff5fae973a9f486
Neer Grover <PaperIsCool5@outlook.com>
2025-02-24 17:32:12 +0300

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  base: '/vuejs-add-calc/', // <-- add this line
+  base: '/vuejs-add-calc/', // <-- make sure this is here
 })
